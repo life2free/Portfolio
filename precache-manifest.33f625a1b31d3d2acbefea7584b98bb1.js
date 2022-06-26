f2fe69134aba7df2048454d63ded870783244c72
shimin <thunderous_0518@hotmail.com>
2022-06-26 09:16:28 -0400

    Updates

---
+++ precache-manifest.33f625a1b31d3d2acbefea7584b98bb1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29462538580b6ca072312679b487df41",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a1ac1ba7ff5149232477",
+    "url": "/Portfolio/static/css/main.26422ab8.chunk.css"
+  },
+  {
+    "revision": "1d565e97b3124eb2b03e",
+    "url": "/Portfolio/static/js/2.6d182d20.chunk.js"
+  },
+  {
+    "revision": "80e109d37ee38a0b1d525ee1bdf87c21",
+    "url": "/Portfolio/static/js/2.6d182d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ac1ba7ff5149232477",
+    "url": "/Portfolio/static/js/main.bb5edff8.chunk.js"
+  },
+  {
+    "revision": "107ce3c625a9fb1de279",
+    "url": "/Portfolio/static/js/runtime-main.e029fae7.js"
+  },
+  {
+    "revision": "28d9d7edb657c69492b34d11c3385ee8",
+    "url": "/Portfolio/static/media/avatar.28d9d7ed.png"
+  },
+  {
+    "revision": "ffa697d4fba0442070be595231ed78f0",
+    "url": "/Portfolio/static/media/backend.ffa697d4.jpeg"
+  },
+  {
+    "revision": "590f2dd35e1aa78059f271e171ade182",
+    "url": "/Portfolio/static/media/componentlibrary.590f2dd3.png"
+  },
+  {
+    "revision": "29ee4b39c118d12b1d3a4ac24997de32",
+    "url": "/Portfolio/static/media/contactbook.29ee4b39.png"
+  },
+  {
+    "revision": "2f86c1513cf6ea6a97416e2e24425dcc",
+    "url": "/Portfolio/static/media/cotripper.2f86c151.png"
+  },
+  {
+    "revision": "707d61a2a8ec0dbf9d989d509ca5bb7a",
+    "url": "/Portfolio/static/media/database.707d61a2.png"
+  },
+  {
+    "revision": "13d8fbc366d81748a8ea2579df8a6529",
+    "url": "/Portfolio/static/media/email.13d8fbc3.png"
+  },
+  {
+    "revision": "70305f6140c0c3163a9e9efb36669d06",
+    "url": "/Portfolio/static/media/endever.70305f61.png"
+  },
+  {
+    "revision": "e9afb6627dbe4cd85c1342b7cbe0d326",
+    "url": "/Portfolio/static/media/framework.e9afb662.png"
+  },
+  {
+    "revision": "1d267581a2ccca17fc11ca714f5b2818",
+    "url": "/Portfolio/static/media/frontend.1d267581.png"
+  },
+  {
+    "revision": "7ae7489ba66e24d1b52b5a67f5539b80",
+    "url": "/Portfolio/static/media/gameofwar.7ae7489b.png"
+  },
+  {
+    "revision": "dc96718636810c179216f4ce11adae45",
+    "url": "/Portfolio/static/media/github.dc967186.png"
+  },
+  {
+    "revision": "f249718251263009dd1bfae551e94ee1",
+    "url": "/Portfolio/static/media/hanoi.f2497182.png"
+  },
+  {
+    "revision": "9ec12de1ea1ba490e7139d3ce14e4a04",
+    "url": "/Portfolio/static/media/nationalparks.9ec12de1.png"
+  },
+  {
+    "revision": "ec36f75ef4261d8ea7c790a1edde56b6",
+    "url": "/Portfolio/static/media/newsassistant.ec36f75e.png"
+  },
+  {
+    "revision": "a3eaf1328ee48c3e234f03b76676bf15",
+    "url": "/Portfolio/static/media/resume.a3eaf132.pdf"
+  },
+  {
+    "revision": "21b12bf9a0257d982cd2c0958b4ed392",
+    "url": "/Portfolio/static/media/tools.21b12bf9.png"
+  }
+]);
